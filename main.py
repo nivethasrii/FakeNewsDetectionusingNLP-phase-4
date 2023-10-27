8b069de79108e8bdcb9388037ad27f31c42d88a0
nivethasrii <146803429+nivethasrii@users.noreply.github.com>
2023-10-27 10:28:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# %% [code] {"execution":{"iopub.status.busy":"2023-10-27T04:25:09.090352Z","iopub.execute_input":"2023-10-27T04:25:09.090818Z","iopub.status.idle":"2023-10-27T04:25:09.111791Z","shell.execute_reply.started":"2023-10-27T04:25:09.090776Z","shell.execute_reply":"2023-10-27T04:25:09.110750Z"}}
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
